bedbab464a8be46c23e5a8c8edd64417ca059a4d
ftyutesefsefesfrfyecgesedvdc <148060461+jose-freire@users.noreply.github.com>
2024-09-23 12:09:15 -0300

    Add files via upload

---
+++ jose/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol Brasileiro',
+    'Quem foi o 1º campeão brasileiro?',
+    'O Primeiro Campeão Brasileiro foi o Vasco'
+)
+
+criaCartao(
+    'Copa do Mundo',
+    'Qual o Primeiro Campeão da Copa do Mundo?',
+    'Uruguai'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual o maior Artilheiro do Futebol??',
+    'Cristiano Ronaldo - 902 gols.'
+)
+
+criaCartao(
+    'Libertadores',
+    'Clube que mais ganhou Libertadores?',
+    ' Club Atlético Independiente'
+)
